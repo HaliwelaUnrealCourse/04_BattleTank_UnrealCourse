100d4648444a0b42ce67dcef5e42f320b276b010
SuperContander\spoter <bobby.haliwela@gmail.com>
2017-09-09 15:32:29 +0700

    BT21 Finding Screen Pixel Coordinates

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -23,13 +23,20 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+private:
 	ATank* GetControlledTank() const;
 
 	// Start the tank moving the barrel so that a shot would hit
 	// where the crosshair intersects the world
 	void AimTowardsCrosshair();
 
-private:
 	// Return an OUT parameter, true if hit landscape
 	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = 0.5;
+
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = 0.3333;
+
 };
