02854ddbb724af2f95408764f654b1c63ce88d24
SuperContander\spoter <bobby.haliwela@gmail.com>
2017-09-18 17:36:37 +0700

    BT41 Using SpawnActor<>() to Spawn

--- BattleTank/Source/BattleTank/Tank.cpp
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -5,6 +5,7 @@
 #include "TankTurret.h"
 #include "TankAimingComponent.h"
 #include "Engine/World.h"
+#include "Projectile.h"
 
 
 // Sets default values
@@ -20,6 +21,7 @@ ATank::ATank()
 void ATank::SetBarrelReference(UTankBarrel* BarrelToSet)
 {
 	TankAimingComponent->SetBarrelReference(BarrelToSet);
+	Barrel = BarrelToSet;
 }
 
 void ATank::SetTurretReference(UTankTurret* TurretToSet)
@@ -50,4 +52,13 @@ void ATank::Fire()
 {
 	auto Time = GetWorld()->GetTimeSeconds();
 	UE_LOG(LogTemp, Warning, TEXT("%f: Tank fires"), Time)
+
+	if (!Barrel) { return; }
+
+	// Spawn a projectile at the socket location on the barrel
+	GetWorld()->SpawnActor<AProjectile>(
+		ProjectileBlueprint,
+		Barrel->GetSocketLocation(FName("Projectile")),
+		Barrel->GetSocketRotation(FName("Projectile"))
+		);
 }
